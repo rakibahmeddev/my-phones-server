0c9e51d562ff1316fcee0ba58d7a412219cb5099
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-05 13:45:02 +0600

    I access phones and also access phone/:id and parseInt(req.params.id) then use find method

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/phones', (req, res) => {
-  res.send(phones);
+    res.send(phones);
+});
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const phone = phones.find(phone => phone.id === id)
+    res.send(phone);
+
 });
 
 app.listen(port, () => {
