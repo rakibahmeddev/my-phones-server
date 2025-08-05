818cf25a3cf0f945294a5a45842a13e3818a54d5
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-08-05 13:39:19 +0600

    phones.json file created with some data and then inside index.js phones variable requires the path and then app.get() and when anyone browse /phones they will get all phones data

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-}); 
+});
+
+app.get('/phones', (req, res) => {
+  res.send(phones);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
